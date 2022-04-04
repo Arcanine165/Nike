7776e484de683692744ca7678c6f40a243949c08
victor_perezvaldez@hotmail.com <victor_perezvaldez@hotmail.com>
2022-04-04 18:32:32 -0500

    Eliminar, vaciar y mas funcionalidades agregadas

--- carrito.js
+++ carrito.js
@@ -7,11 +7,12 @@ function startEventListeners(){
         agregarAlHtml();
         console.log(carritoCompras)
     })
+    table.addEventListener('click',eliminarElemento)
 }
 
 
 function agregarAlHtml(){
-    
+    limpiarHtml();
     carritoCompras.forEach(producto => {
         const col = document.createElement('tr');
         
@@ -21,11 +22,36 @@ function agregarAlHtml(){
             <td><p>${producto.name}</p></td>
             <td><p>${producto.price}</p></td>
             <td><p>${producto.cantidad}</p></td>
-            <td><button type="button" class="btn btn-danger">Eliminar</button></td>
+            <td><button type="button" class="btn btn-danger" id=${producto.id}>Eliminar</button></td>
         `
         table.appendChild(col)
     })
     
 }
+function eliminarElemento(e){
+    e.preventDefault();
+    
+    if(e.target.type == 'button'){
+        const index = carritoCompras.findIndex(element => e.target.getAttribute('id') == element.id);
+        console.log(e.target)
+        if(carritoCompras[index].cantidad > 1){
+            let precioOriginal = carritoCompras[index].price / carritoCompras[index].cantidad;
+            carritoCompras[index].cantidad--;
+            carritoCompras[index].price = precioOriginal * carritoCompras[index].cantidad;
+            
+        }else{
+            carritoCompras.splice(index,1);
+        }
+        saveInLocalStorage();
+        limpiarHtml();
+        agregarAlHtml();
+    }
+    
+}
+function limpiarHtml(){
+    table.innerHTML = '';
+}
 
-
+function saveInLocalStorage(){
+    localStorage.setItem('carrito',JSON.stringify(carritoCompras));
+}
